8dafd174c9498b88f1a14ee23411efa43b43c45a
ravikant706 <ravikant706@gmail.com>
2023-09-20 17:19:42 +0530

    seconde dicee js code upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImages1 = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll('img')[0].setAttribute("src", randomImages1);
+
+// Seconde dicee code
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImages2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll('img')[1].setAttribute('src', randomImages2);
